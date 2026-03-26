8ee7c683c80458a2f4829ca016842d2789b353ed
Nithin G <139322903+17nithinnayak@users.noreply.github.com>
2026-03-26 23:35:13 +0530

    Create prog3.cpp for pointer demonstration
    Add a simple C++ program demonstrating pointer usage.

---
+++ pointer/prog3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[] = { 4, 5, 6, 7 };
+    int* p = (arr + 1);
+    cout << *arr + 10;
+    cout<<*p;
+    return 0;
+}
+
+# Output: 14 5
